402d8cbb2a29766f313034c986e2041018532671
viandavisual <viandavisual@gmail.com>
2026-09-25 12:57:57 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,61 @@
+const CACHE='carpesanos-v21';
+const CORE=[
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './assets/materials/binder-vinyl-neutral.png',
+  './assets/materials/binder-wear-overlay.png',
+  './assets/materials/label-paper.png',
+  './assets/materials/metal-ring.png',
+  './assets/materials/shelf-wood-back.png',
+  './assets/materials/shelf-wood-front.png',
+  './assets/materials/shelf-wood-top.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(CORE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        if (response && (response.ok || response.type === 'opaque')) {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      });
+    })
+  );
+});
